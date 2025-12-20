a36696f39443cada8073a3eaba4192a09faf360d
firesaspent <firesaspent@gmai.com>
2025-12-20 13:12:17 +0300

    add libraries for dependency check and SOBM

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/gogo/protobuf/proto"
+	"github.com/gorilla/websocket"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,4 +19,24 @@ func main() {
 
 	fmt.Println("Server started on port 8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+func JWTlib() string {
+	token := jwt.New(jwt.SigningMethodHS256)
+	ss, _ := token.SignedString([]byte("secret"))
+
+	return ss
+}
+
+func fakeGIN() {
+	r := gin.Default()
+	_ = r
+}
+
+func mockWebsocket() {
+	_ = websocket.IsCloseError
+}
+
+func mockProtoBuff() {
+	_ = proto.ErrInternalBadWireType
 }
